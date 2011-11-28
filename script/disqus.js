a25785d2e488959cb627d0f558780ae0e84d2664
Christopher Wellons <mosquitopsu@gmail.com>
2011-11-28 14:18:54 -0500

    Put scripts out into their own files.

---
+++ script/disqus.js
@@ -0,0 +1,7 @@
+// Disqus comments
+
+(function() {
+    var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+    dsq.src = 'http://' + disqus_shortname + '.disqus.com/embed.js';
+    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+})();
