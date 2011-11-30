84b646ad0f41af637c365017c9cae7296c54b279
Christopher Wellons <mosquitopsu@gmail.com>
2011-11-30 13:35:42 -0500

    Add cookie support and selector indication.

--- script/theme.js
+++ script/theme.js
@@ -3,14 +3,24 @@ function disableAllThemes() {
     $(".theme[rel='stylesheet']").each(function() {
         $(this).attr("disabled", true);
     });
+    $(".theme-button").each(function() {
+        $(this).removeClass("selected");
+    });
 }
 
 /* Set the theme by index number. */
 function setTheme(index) {
     disableAllThemes();
     $(".theme[rel='stylesheet']").eq(index).attr("disabled", false);
+    $(".theme-button").eq(index).addClass("selected");
+    $.cookies.set("theme", index);
 }
 
 $("document").ready(function() {
-    setTheme(0); // Enable the default theme.
+    var save = $.cookies.get("theme");
+    if (save) {
+        setTheme(save);
+    } else {
+        setTheme(0); // Enable the default theme.
+    }
 });
