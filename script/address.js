2aa83b79ee03af07dc5f3f0fde3eb3b45b0ddde7
Christopher Wellons <wellons@nullprogram.com>
2013-11-25 22:09:05 -0500

    Drop the tip jar.

--- script/address.js
+++ script/address.js
@@ -4,8 +4,4 @@
 $(document).ready(function() {
     var email = 'wellons\u0040nullprogram.com';
     $('#email').attr('href', 'mailto:' + email).text(email);
-
-    var bitcoin = '17YcJGMHjeTqEE9k1XKnC2PENBAtuoz8GQ';
-    $('#bitcoin-qrcode').qrcode({height: 125, width: 125, text: bitcoin});
-    $('#bitcoin-address').text(bitcoin);
 });
