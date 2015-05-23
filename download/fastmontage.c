c21664c71621807ad7ea6026f0fc80a749e1d432
Christopher Wellons <wellons@nullprogram.com>
2015-05-23 12:02:13 -0400

    Add command line option to montage "script."

--- download/fastmontage.c
+++ download/fastmontage.c
@@ -1,13 +1,17 @@
 /**
  * Reads concatenated PPMs (netpbm) on stdin and writes the montage as
- * a PPM to stdout. Puts 60 images per row and only takes a few
- * milliseconds.
+ * a PPM to stdout. Places 60 images per row (default); see -w option.
  *
  * Compile program:
- *   $ gcc -std=c99 -O3 -o fastmontage fastmontage.c
+ *   $ cc -std=c99 -O3 -o fastmontage fastmontage.c
  *
- * Video montage:
+ * Video montage (with avconv + ImageMagick):
  *   $ avconv -i video.mp4 -s:v 32x18 -r 1 -c:v ppm -f image2pipe - | \
+ *         ./fastmontage | convert - montage.png
+ *
+ * Image montage (with ImageMagick):
+ *   $ find frames/ -iname '*.jpg' -print0 | sort -z | \
+ *         xargs -0 -I {} convert {} ppm:- | \
  *         ./fastmontage | convert - montage.png
  *
  * Warning: The PPM reader is incomplete as it doesn't tolerate PPM
@@ -19,13 +23,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <getopt.h>
 
 #ifdef _WIN32
 #include <io.h>
 #include <fcntl.h>
 #endif
-
-#define WIDTH 60
 
 typedef struct image {
     long width;
@@ -88,29 +91,56 @@ image_write(const image *im)
     fwrite(im->colors, im->width * im->height, 3, stdout);
 }
 
+static void
+print_usage(char *program)
+{
+    printf("Usage: %s [-w <num_frames>] [-h]\n", program);
+    puts("  -w <num_frames>   Input frames per row (60)");
+    puts("  -h                Print usage information\n");
+    puts("Reads concatenated PPMs on stdin, writes montage PPM to stdout.");
+}
+
 int
-main(void)
+main(int argc, char **argv)
 {
     #ifdef _WIN32
     _setmode(fileno(stdout), _O_BINARY);
     _setmode(fileno(stdin), _O_BINARY);
     #endif
 
+    /* Options */
+    int frames_wide = 60;
+    int option;
+    while ((option = getopt(argc, argv, "w:h")) != -1) {
+        switch (option) {
+            case 'w':
+                frames_wide = atoi(optarg);
+                break;
+            case 'h':
+                print_usage(argv[0]);
+                return EXIT_SUCCESS;
+            default:
+                print_usage(argv[0]);
+                return EXIT_FAILURE;
+        }
+    }
+
     image *frame = image_read(NULL);
-    image *final = image_create(frame->width * WIDTH, frame->height);
-    image_copy(final, 0, 0, frame);
+    image *montage = image_create(frame->width * frames_wide, frame->height);
+    image_copy(montage, 0, 0, frame);
     long count = 1;
     while (image_read(frame)) {
-        long x = count % WIDTH;
-        long y = count / WIDTH;
-        if ((y + 1) * frame->height > final->height)
-            final = image_grow(final, final->height * 2);
-        image_copy(final, x * frame->width, y * frame->height, frame);
+        long x = count % frames_wide;
+        long y = count / frames_wide;
+        if ((y + 1) * frame->height > montage->height)
+            montage = image_grow(montage, montage->height * 2);
+        image_copy(montage, x * frame->width, y * frame->height, frame);
         count++;
     }
-    final = image_grow(final, ((count + WIDTH - 1) / WIDTH) * frame->height);
-    image_write(final);
-    free(final);
+    long frames_high = (count + frames_wide - 1) / frames_wide;
+    montage = image_grow(montage, frames_high * frame->height);
+    image_write(montage);
+    free(montage);
     free(frame);
     return 0;
 }
