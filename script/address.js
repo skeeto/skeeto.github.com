2ae02d8c2e4a8cc31856b423361bfaf91ba2af71
Christopher Wellons <wellons@nullprogram.com>
2016-09-04 22:24:28 -0400

    Update to simpler layout.

--- script/address.js
+++
@@ -1,7 +0,0 @@
-/* Fills in various address information on the page.
- */
-
-$(document).ready(function() {
-    var email = 'wellons\u0040nullprogram.com';
-    $('#email').attr('href', 'mailto:' + email).text(email);
-});
