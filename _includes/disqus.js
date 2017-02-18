dde6fccaa9a15c391f4f01b06cd2cde0de823c9a
Christopher Wellons <wellons@nullprogram.com>
2017-02-18 13:05:30 -0500

    Hide Disqus behind a load button.
    The slowest elements on the page by far is Disqus. This should
    dramatically improve page performance.

--- _includes/disqus.js
+++ _includes/disqus.js
@@ -1,5 +1,9 @@
-(function() {
-    var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
-    dsq.src = 'http://' + disqus_shortname + '.disqus.com/embed.js';
-    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
-})();
+function load_comments(div) {
+    var dsq = document.createElement('script');
+    dsq.type = 'text/javascript';
+    dsq.async = true;
+    dsq.src = '//nullprogram.disqus.com/embed.js';
+    (document.getElementsByTagName('head')[0] ||
+     document.getElementsByTagName('body')[0]).appendChild(dsq);
+    div.parentNode.removeChild(div);
+}
